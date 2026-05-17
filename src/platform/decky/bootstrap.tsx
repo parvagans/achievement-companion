bd5f0de44a7cab777e599bbf4c7fe062253709e8
Adam <ainglert25@gmail.com>
2026-05-17 16:48:38 +0200

    chore: bump version to 0.2.4

--- src/platform/decky/bootstrap.tsx
+++ src/platform/decky/bootstrap.tsx
@@ -75,7 +75,7 @@ interface SteamLibraryScanActionState {
 
 type ProviderLauncherTone = "connected" | "setup" | "neutral";
 
-const ACHIEVEMENT_COMPANION_VERSION = "0.2.3";
+const ACHIEVEMENT_COMPANION_VERSION = "0.2.4";
 
 function getChooserCardStyle(): CSSProperties {
   return {
